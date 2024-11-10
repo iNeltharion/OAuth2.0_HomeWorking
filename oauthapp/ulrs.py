4e086c1b86bbfe442d76e40311aaa8fba83fc4c2
iNeltharion <an121@ymail.com>
2024-11-10 12:55:11 +0300

    Create_app

---
+++ oauthapp/ulrs.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from oauth_project.urls import urlpatterns
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('social/', include('social_django.urls', namespace='social_auth')),
+]
